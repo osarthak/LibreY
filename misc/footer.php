0fdd0fad42cfa1fdc7dba5fd15c8d1f4afe64111
osarthak <hi99@tutanota.com>
2023-09-20 21:33:05 +0530

    rm api access for everyone

--- misc/footer.php
+++ misc/footer.php
@@ -3,7 +3,7 @@
     <a href="https://github.com/Ahwxorg/librey/" target="_blank">Source</a>
     <a href="./instances.php" target="_blank">Instances</a>
     <a href="./settings.php">Settings</a>
-    <a href="./api.php" class="hide" target="_blank">API</a>
+    <a href="./api.php" target="_blank">API</a>
     <a href="./donate.php">Donate ❤️</a>
 </div>
 <div class="git-container hide">
