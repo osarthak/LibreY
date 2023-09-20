b778bb0028a55d04d495dc1d05dc778c3f955493
osarthak <hi99@tutanota.com>
2023-09-20 21:09:04 +0530

    rm api access for everyone for bot protection

--- api.php
+++ api.php
@@ -1,23 +1,23 @@
 <!-- <?php
-    require "misc/tools.php";
-    require "misc/search_engine.php";
+    // require "misc/tools.php";
+    // require "misc/search_engine.php";
 
-    $opts = load_opts();
+    // $opts = load_opts();
 
-    if (!$opts->query) {
-        echo "<p>Example API request: <a href=\"./api.php?q=gentoo&p=2&t=0\">./api.php?q=gentoo&p=2&t=0</a></p>
-        <br/>
-        <p>\"q\" is the keyword</p>
-        <p>\"p\" is the result page (the first page is 0)</p>
-        <p>\"t\" is the search type (0=text, 1=image, 2=video, 3=torrent, 4=tor)</p>
-        <br/>
-        <p>The results are going to be in JSON format.</p>
-        <p>The API supports both POST and GET requests.</p>";
+    // if (!$opts->query) {
+    //     echo "<p>Example API request: <a href=\"./api.php?q=gentoo&p=2&t=0\">./api.php?q=gentoo&p=2&t=0</a></p>
+    //     <br/>
+    //     <p>\"q\" is the keyword</p>
+    //     <p>\"p\" is the result page (the first page is 0)</p>
+    //     <p>\"t\" is the search type (0=text, 1=image, 2=video, 3=torrent, 4=tor)</p>
+    //     <br/>
+    //     <p>The results are going to be in JSON format.</p>
+    //     <p>The API supports both POST and GET requests.</p>";
 
-        die();
-    }
+    //     die();
+    // }
 
-    $results = fetch_search_results($opts, false);
-    header("Content-Type: application/json");
-    echo json_encode($results);
+    // $results = fetch_search_results($opts, false);
+    // header("Content-Type: application/json");
+    // echo json_encode($results);
 ?> -->
