160e98ab289fb91b45e9a6a625c84675804d9e62
Sectorfive <153916716+SectorV5@users.noreply.github.com>
2024-01-23 20:37:25 +0100

    Add Serbian Locale.

---
+++ locale/rs.php
@@ -0,0 +1,56 @@
+<?php
+
+return array(
+   "page_title" => "LibreY Претрага",
+   "search_button" => "Претражи са LibreY-ем",
+   "torrent_search_button" => "Претражи торенте са LibreY-ем",
+
+   "source_code_link" => "Изворни код",
+   "instances_link" => "Инстанце",
+   "settings_link" => "Подешавања",
+   "api_link" => "API",
+   "donate_link" => "Донирајте ❤️",
+
+   "latest_commit" => "Последњи комит: %s",
+
+   "category_general" => "Опште",
+   "category_images" => "Слике",
+   "category_videos" => "Видео снимци",
+   "category_torrents" => "Торенти",
+   "category_tor" => "Tor",
+   "category_maps" => "Мапе",
+
+   "feature_disabled" => "Оператор је онемогућио ову функцију :C",
+
+   "settings_title" => "Подешавања",
+   "settings_theme" => "Тема",
+   "settings_special_disabled" => "Онемогући посебне упите (нпр.: конверзија валута)",
+
+   "settings_frontends" => "Приватни фронтендови",
+   "settings_frontends_description" => "На пример, ако желите да гледате YouTube са приватношћу, кликните на „Invidious“, пронађите инстанцу која вам највише одговара, а затим је налепите (исправан формат: https://example.rs)",
+   "settings_frontends_disable" => "Онемогући фронтендове",
+
+   "settings_search_settings" => "Подешавања претраге",
+   "settings_language" => "Језик",
+   "settings_preferred_engine" => "Преферирани претраживач",
+
+   "settings_number_of_results" => "Број резултата по страници",
+
+   "settings_safe_search" => "Безбедна претрага",
+   "settings_save" => "Сачувај",
+   "settings_reset" => "Ресетуј",
+
+
+   "failure_fallback" => "Нема резултата. Покушај пребацивања на другу инстанцију није успео.",
+   "failure_empty" => "Нема резултата. Пробајте са другачијим кључним речима!",
+   "result_no_description" => "Нису дате довољно информације о овој Веб страници.",
+
+   "instances_librex" => "Следеће инстанце раде на старијој верзији %s",
+
+   "donate_original_developer" => "Донирајте програмеру који је развио пројекат %s, који LibreY покушава да побољша",
+   "donate_fork" => "Донирајте особи која је направила форк %s у LibreY",
+
+   "api_unavailable" => "Овај LibreY API тренутно није доступан"
+);
+
+?>
